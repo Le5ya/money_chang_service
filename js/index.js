64c5e379222d0a2500bf5a33e64f9c65b0b484da
Le5ya <liliatolstyh@yandex.ru>
2022-07-22 20:06:51 +0300

    finish

--- js/index.js
+++ js/index.js
@@ -2,4 +2,6 @@ import './modul/menu.js'
 import './modul/modal.js';
 import './modul/tabs_about_us.js';
 import './modul/exchange_rate.js';
+import { accordion } from './modul/faq.js';
 
+accordion();
